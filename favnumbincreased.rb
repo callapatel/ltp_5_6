31eee147962343872648a87ed36aaf785508f34f
Calla Patel <callapatel@gmail.com>
2014-09-03 19:48:04 -0700

    first

---
+++ favnumbincreased.rb
@@ -0,0 +1,7 @@
+
+puts 'Please tell me your fav number ever: '
+number_fav = gets.chomp
+
+bigger_num = number_fav.to_i + 1
+
+puts 'may I suggest: ' + bigger_num.to_s 
---
+++ fullname.rb
@@ -0,0 +1,13 @@
+
+
+puts 'Hi There!'
+puts 'What is your first name?: '
+first_name = gets.chomp
+puts 'Thank you. Now what is your middle name?: '
+mid_name = gets.chomp
+puts 'Ah! Lastly I\'ll need to know your last name please: '
+last_name = gets.chomp
+
+puts 'well its a miracle, I believe I can tell you your entire name.'
+puts first_name + ' ' + mid_name + ' ' + last_name + ' '
+puts 'I just wanted to say hello.. '
